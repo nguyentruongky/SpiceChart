872b661651becd057e1ef065ec5bec04c0354d41
Scott Damopoulos <scottdamopoulos@scotts-mbp.attlocal.net>
2019-03-31 14:25:46 -0400

    Before starting session

---
+++ Spice Chart/ElementEditor.swift
@@ -0,0 +1,19 @@
+//
+//  ElementEditor.swift
+//  Spice Chart
+//
+//  Created by Scott Damopoulos on 3/31/19.
+//  Copyright © 2019 damopoulos.scott@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ElementEditor: UIView {
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet var valueLabel: UIView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var stepper: UIStepper!
+    
+}
